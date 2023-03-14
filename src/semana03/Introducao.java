23a5d00f3fc3d41a4accc9b8130587feeff4ce64
Estudante <v.leticia@aluno.ifsp.edu.br>
2023-03-14 17:51:56 -0300

    Estrutura de cot[
    git status

---
+++ src/semana03/Introducao.java
@@ -0,0 +1,20 @@
+package semana03;
+
+public class Introducao {
+    public static void main(String [] args){
+        //Comentário de uma linha
+
+        /*
+         * Comentário de várias
+         *  linhas
+         */
+
+         //Tipos primitivos
+         //Inteiros
+         //byte, short, int, long
+         //1 bytes- -126
+    }
+
+
+
+}
